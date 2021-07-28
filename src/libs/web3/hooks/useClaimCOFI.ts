ea39dde803fb03996641d41a7ea428e0a8503f3a
nest-coder <nest-coder@nestprotocol.org>
2021-07-28 21:13:06 +0800

    feat(contract): update address

--- src/libs/web3/hooks/useClaimCOFI.ts
+++ src/libs/web3/hooks/useClaimCOFI.ts
@@ -4,7 +4,7 @@ import useTransaction, { TransactionType, TransasctionClaimCOFIContent } from '.
 import useWeb3 from './useWeb3'
 
 const useClaimCOFI = (content: TransasctionClaimCOFIContent) => {
-  const { api } = useWeb3()
+  const { api, inited } = useWeb3()
   const { push } = useTransaction()
 
   const handler = useCallback(async () => {
@@ -14,14 +14,14 @@ const useClaimCOFI = (content: TransasctionClaimCOFIContent) => {
         content,
       },
       async () => {
-        if (!api || !content.amount || !api.Tokens.CNODE.address) {
+        if (!api || !api.Tokens.CNODE.address) {
           return
         }
 
         return api.Contracts.CoFiXVaultForStaking.contract?.getReward(api.Tokens.CNODE.address)
       }
     )
-  }, [api, content, content.amount])
+  }, [api, inited, content])
 
   return { handler }
 }
